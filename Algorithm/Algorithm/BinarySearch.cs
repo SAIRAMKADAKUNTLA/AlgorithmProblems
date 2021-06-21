a29ac41420114b40a46ca9d31b7cbc6432eb13d7
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-21 20:52:39 +0530

    [Sairam]Added:inorder to search the word index using the binary search

---
+++ Algorithm/Algorithm/BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public class BinarySearch
+    {
+        public int search(string[]array,string data)
+        {
+            int left = 0, right = array.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + right / 2;
+                int check = data.CompareTo(array[mid]);
+                if (check == 0)
+                    return mid;
+                if (check > 0)
+                    return mid + 1;
+                else
+                    return mid - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to algorithm problems");
+            string[] array = { "hi", "hello", "sai", "ram", "done" };
+            string data = "sai";
+            BinarySearch binary = new BinarySearch();
+            int result = binary.search(array, data);
+            if(result == -1)
+                Console.WriteLine("NO ELEMENTS FOUND");
+            else
+                Console.WriteLine("Elements found at index"+result);
+        }
+    }
+}
